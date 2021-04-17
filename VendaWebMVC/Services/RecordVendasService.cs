27d5b99b3214555ac59f29ec71479eca8e232c8d
Diego Fernando da Silva <diego_fernandosm@hotmail.com>
2021-04-17 11:31:58 -0300

    Serach for group   FIM !

--- VendaWebMVC/Services/RecordVendasService.cs
+++ VendaWebMVC/Services/RecordVendasService.cs
@@ -36,5 +36,25 @@ namespace VendaWebMVC.Services
                 .ToListAsync();
         }
 
+        public async Task<List<IGrouping<Department, RecordVendas>>> FindByDateGroupingAsync(DateTime? minDate, DateTime? maxDate)
+        {
+            var result = from obj in _context.RecordVendas select obj;
+
+            if (minDate.HasValue)
+            {
+                result = result.Where(x => x.Data >= minDate.Value);
+            }
+            if (maxDate.HasValue)
+            {
+                result = result.Where(x => x.Data <= maxDate.Value);
+            }
+            return await result.Include(x => x.Vendedor)
+                .Include(x => x.Vendedor.Department)
+                .OrderByDescending(x => x.Data)
+                .GroupBy(x => x.Vendedor.Department)
+                .ToListAsync();
+        }
+
+
     }
 }
