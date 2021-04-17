8ceee7932eb0f87b65592b76fc5366630515f1b8
Diego Fernando da Silva <diego_fernandosm@hotmail.com>
2021-04-17 11:39:47 -0300

    formatando search grouping fim

--- VendaWebMVC/Models/RecordVendas.cs
+++ VendaWebMVC/Models/RecordVendas.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using VendaWebMVC.Models.Enums;
@@ -9,8 +10,15 @@ namespace VendaWebMVC.Models
     public class RecordVendas
     {
         public int Id { get; set; }
+
+
+        [DisplayFormat(DataFormatString = "{0: dd/MM/yyyy}")]
         public DateTime Data { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double Total { get; set; }
+
+
         public StatusVenda Status { get; set; }
         public Vendedor Vendedor { get; set; }
 
